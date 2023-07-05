18b72fa6c20938fcd60c58a6c3974333637e6459
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-05 14:09:24 +0100

    added more test

--- index.tests.js
+++ index.tests.js
@@ -24,40 +24,73 @@ function notEqual(actual, expected, message) {
   }
 }
 
-test('Submitting a task should add task to list', () => {
+// test('Submitting a task should add task to list', () => {
+//   // Get all inputs
+//   const toDoContainer = document.getElementById('to-do-container');
+//   const taskInput = document.getElementById('task-input');
+//   // Write the task
+//   taskInput.value = 'testing 123';
+
+//   // Submit the task
+//   const submitButton = document.querySelector('button[type="submit"]');
+//   submitButton.click();
+
+//   // Check that the task was added
+//   const result = toDoContainer.firstChild;
+//   equal(result.textContent, 'testing 123');
+// })
+
+test("Adding tasks", () => {
   // Get all inputs
-  const toDoContainer = document.getElementById('to-do-container');
-  const taskInput = document.getElementById('task-input');
+  const toDoContainer = document.getElementById("to-do-container");
+  const taskInput = document.getElementById("task-input");
   // Write the task
-  taskInput.value = 'testing 123';
+  taskInput.value = "testing 123";
 
   // Submit the task
   const submitButton = document.querySelector('button[type="submit"]');
   submitButton.click();
 
   // Check that the task was added
-  const result = toDoContainer.firstChild;
-  equal(result.textContent, 'testing 123');
-})
+  const result = toDoContainer.getElementsByTagName("li")[0];
+  equal(result.textContent, "testing 123");
 
-test('Clicking on a task should strike it through', () => {
+  // Add another task
+  taskInput.value = "testing 456";
+
+  // Submit the task
+  const submitButton2 = document.querySelector('button[type="submit"]');
+  submitButton2.click();
+
+  // Check that the task was added
+  const result2 = toDoContainer.getElementsByTagName("li")[1];
+  equal(result2.textContent, "testing 456");
+
+  //Check total number of tasks
+  equal(toDoContainer.getElementsByTagName("li").length, 2);
+
+  // Remove the second task
+  result2.remove();
+});
+
+test("Clicking on a task should strike it through", () => {
   // Get all inputs
-  const testTask = document.toDoContainer.firstChild;
+  const testTask = document.toDoContainer.getElementsByTagName("li")[0];
 
   // Click on the task
   testTask.click();
 
   // Check that the task was striked through
-  const result = testTask.classList.contains('strike-through');
+  const result = testTask.classList.contains("strike-through");
   equal(result, true);
 
   // Click again
   testTask.click();
 
   // Check that the task is not striked through
-  const result2 = testTask.classList.contains('strike-through');
+  const result2 = testTask.classList.contains("strike-through");
   equal(result2, false);
 
   // Remove the task
   testTask.remove();
-})
+});
